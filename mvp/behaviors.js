a5f12b4bf714c02f792d2202df75d4edb99c40f0
Tor Hagemann <tohagema@cisco.com>
2018-03-28 06:56:42 -0700

    mvp: don't book anything for email not @cisco.com

--- mvp/behaviors.js
+++ mvp/behaviors.js
@@ -1,23 +1,43 @@
 const { getEventBus } = require('./firehose.js')
 
-const hackathonDemo = (events = getEventBus()) => {
+const hackathonDemo = (firehose = getEventBus()) => {
 
-	events.on('spark:*',
+	const denyEmailAddress = async ({ clients, roomId }) => {
+		const english = 'Sorry, but I only answer to Cisco employees.'
+		return clients.cisco.spark.message.create({ roomId, text: english })
+	}
+
+	const recoverEnglish = async ({ text }) => {
+		return text
+	}
+
+	const replyEnglish = async ({ text }) => {
+		return text
+	}
+
+	firehose.on('spark:*',
 		async function log ({ data }) {
-			events.log.info({ data }, 'just FYI')
+			firehose.log.info({ data }, 'just FYI')
 		})
 
-	events.on('spark:messages:created',
+	firehose.on('spark:messages:created',
 		async function book ({ clients, data }) {
-			const { roomId, text } = await clients.cisco.spark.messages.get(data.id)
-			const { data: resources } = await clients.google.listCalendarResources()
-			const { data: event } = await clients.google.createCalendarEvent()
-			const { data: list } = await clients.google.listCalendarEvents()
-			const message = await clients.cisco.spark.messages.create({ roomId, text })
-			events.log.info({ event, list, message, resources }, 'responded to message')
+			const { personEmail, roomId, text } = await clients.cisco.spark.messages.get(data.id)
+			if (!personEmail.endsWith('@cisco.com')) return denyEmailAddress({ clients, roomId })
+			try {
+				const { data: events } = await clients.google.listCalendarEvents()
+				const { data: resources } = await clients.google.listCalendarResources()
+				const reply = await replyEnglish({ clients, events, resources, text })
+				const english = reply ? reply : await recoverEnglish({ clients, text })
+				await clients.cisco.spark.messages.create({ roomId, text: english })
+			} catch (error) {
+				const english = 'Sorry, something went wrong; please try that again.'
+				await clients.cisco.spark.messages.create({ roomId, text: english })
+				throw error
+			}
 		})
 
-	return events
+	return firehose
 
 }
 
